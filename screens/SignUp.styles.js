4ae043e79753d15854a0fc462e97a6facf2c0df6
Awadje <michaelawad85@gmail.com>
2017-05-31 20:57:03 +0200

    Add styling to button

--- screens/SignUp.styles.js
+++ screens/SignUp.styles.js
@@ -12,4 +12,19 @@ export default StyleSheet.create({
     alignSelf: 'center',
     marginBottom: 30
   },
+  buttonText: {
+  fontSize: 18,
+  color: 'white',
+  alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
 });
