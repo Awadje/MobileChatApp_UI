a52d6f59abdd8d48333f82e7e6a385d27a0ce848
Awadje <michaelawad85@gmail.com>
2017-05-31 22:00:48 +0200

    Add middleware file

---
+++ store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import { composeWithDevTools } from 'remote-redux-devtools';
+import apiMiddleware from './middleware/api';
+import * as reducers from './reducers'
+
+const reducer = combineReducers(Object.assign({}, reducers))
+
+const enhancer = composeWithDevTools(
+  applyMiddleware(ReduxThunk),
+  applyMiddleware(apiMiddleware),
+)
+
+const store = createStore(reducer, enhancer)
+
+export default store
