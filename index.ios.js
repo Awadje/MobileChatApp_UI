8a2fb2380febfe796cb5003b87f07d4088976d24
Awadje <michaelawad85@gmail.com>
2017-05-31 20:47:15 +0200

    Add person component to create form

--- index.ios.js
+++ index.ios.js
@@ -3,11 +3,8 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
-
  import { AppRegistry } from 'react-native';
  import ShatApp from './ShatApp';
-
 
 
 AppRegistry.registerComponent('ShatApp', () => ShatApp);
